7058d90723019863ae126b83a1c9e2d74c7f3f1f
SRIKARTIK MATETI <matetikartik1999@gmail.com>
2025-08-13 15:33:13 +0530

    Update HomeController.java

--- src/test/java/com/example/demo/HomeController.java
+++ src/test/java/com/example/demo/HomeController.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "index";
+        return "<h1>✅ Jenkins Build Successful!</h1><p>Your Spring Boot app is running on port 9090.</p>";
     }
 }
