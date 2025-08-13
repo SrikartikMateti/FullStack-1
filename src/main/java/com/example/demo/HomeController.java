a43df13175a94687c682aacf83a1d4f75f6b4636
SRIKARTIK MATETI <matetikartik1999@gmail.com>
2025-08-13 15:35:30 +0530

    Create HomeController.java

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>✅ Jenkins Build Successful!</h1><p>Spring Boot is running on port 9090.</p>";
+    }
+}
